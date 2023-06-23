ed1e6709426dada689db8bcfc8660693c2906988
B3n-d1v3 <81184173+B3n-d1v3@users.noreply.github.com>
2023-06-23 08:41:12 +0200

    script and css style updates

---
+++ script.js
@@ -0,0 +1,11 @@
+(() => {
+  'use strict'
+
+   if (!(/windows phone/i.test(navigator.userAgent)) && /android/i.test(navigator.userAgent)) {
+     window.location.href = 'itms-apps://play.google.com/store/apps/details?id=com.motionvolt.flipdiving'
+   }
+
+  if (/ipad|iphone|ipod/i.test(navigator.userAgent)) {
+    window.location.href = 'itms-apps://apps.apple.com/app/diving-rules/id1579861867'
+  }
+}
