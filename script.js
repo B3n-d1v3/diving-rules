f9f4a69f6d02690ada22e9e5478964fb944a3685
B3n-d1v3 <81184173+B3n-d1v3@users.noreply.github.com>
2023-06-23 16:39:14 +0200

    Update script.js redirect cleanup

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// $(document).ready(function () {
-
-        console.log('chargé');
-        
-        if (navigator.userAgent.toLowerCase().indexOf("android") > -1) {
-            window.location.href = 'market://details?id=com.motionvolt.flipdiving';
-        }
-        if (navigator.userAgent.toLowerCase().indexOf("iphone") > -1) {
-            window.location.href = 'itms-apps://apps.apple.com/app/diving-rules/id1579861867';
-        }
-//   });
+ if (navigator.userAgent.toLowerCase().indexOf("android") > -1) {
+        window.location.href = 'market://details?id=com.motionvolt.flipdiving';
+}
+if (navigator.userAgent.toLowerCase().indexOf("iphone") > -1) {
+        window.location.href = 'itms-apps://apps.apple.com/app/diving-rules/id1579861867';
+}
