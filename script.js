4d6ce1d93e2875afc375cb9e31b5c925150fdc44
B3n-d1v3 <81184173+B3n-d1v3@users.noreply.github.com>
2023-08-10 09:49:53 +0200

    Update script.js
    Changed the play store link

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  if (navigator.userAgent.toLowerCase().indexOf("android") > -1) {
-        window.location.href = 'market://details?id=com.motionvolt.flipdiving';
+        window.location.href = 'market://details?id=com.bendivingjudge.divingrules';
 }
 if (navigator.userAgent.toLowerCase().indexOf("iphone") > -1) {
         window.location.href = 'itms-apps://apps.apple.com/app/diving-rules/id1579861867';
